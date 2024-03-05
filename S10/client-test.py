ef6730b9824b9a7088009622cc306f859a591885
jquijada05 <j.quijada.2023@alumnos.urjc.es>
2024-03-05 18:08:36 +0100

    Commit

--- S10/client-test.py
+++ S10/client-test.py
@@ -2,5 +2,8 @@ from Client0 import Client
 IP = "212.128.255.95"
 PORT = 8081
 c = Client(IP, PORT)
-print(f"To server: {msg}")
-print(f"From server: {c.talk(new_msg)}")
+for i in range(5):
+    msg = "Message " + str(i)
+    response = c.talk(msg)
+    print(f"To server: {msg}")
+    print(f"From server: {response}")
