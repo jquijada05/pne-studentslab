7fde98fdbf35931f430942d97a2c40ac2e6ec451
jquijada05 <j.quijada.2023@alumnos.urjc.es>
2024-03-07 16:00:03 +0100

    Commit

--- P03/SeqServer.py
+++ P03/SeqServer.py
@@ -1,8 +1,9 @@
 import termcolor
 import socket
+from Seq1 import Seq
 class SeqServer:
     def __init__(self):
-        self.PORT = 8080
+        self.PORT = 8081
         self.IP = "127.0.0.1"
         ls = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         ls.bind((self.IP, self.PORT))
@@ -23,14 +24,59 @@ class SeqServer:
 
     def return_response(self, msg):
         if msg == "PING":
-            termcolor.cprint("PING command!", "green")
             return self.ping_response()
-
+        elif msg.startswith("GET"):
+            return self.get_response(msg)
+        elif msg.startswith("INFO"):
+            return self.get_info(msg)
+        elif msg.startswith("COMP"):
+            return self.get_comp(msg)
+        elif msg.startswith("REV"):
+            return self.get_rev(msg)
 
     def ping_response(self):
+        termcolor.cprint("PING command!", "green")
+        print("OK!")
         return "OK!\n"
 
+    def get_response(self, msg):
+        termcolor.cprint("GET", "green")
+        sequences = ["TCAGTCAA", "CGATACGA", "CCAGTGCA", "TTTCAGTA", "CATGCTAG"]
+        for i in msg:
+            if i.isdigit():
+                if 0 <= int(i) <= 4:
+                    n = sequences[int(i)]
+                    print(n)
+                    return n
+                else:
+                    return "No sequence exists for this number"
 
+
+    def get_info(self, msg):
+        termcolor.cprint("INFO", "green")
+        sequence = Seq(msg.replace("INFO", "").strip())
+        info = f"Sequence: {sequence}\n"
+        info += f"Total length: {sequence.len()}\n"
+        for base in ["A", "C", "G", "T"]:
+            count = sequence.count_base(base)
+            percentage = count / sequence.len() * 100 if sequence.len() > 0 else 0
+            info += f"{base}: {count} ({percentage}%)\n"
+        print(info)
+        return info
+
+    def get_comp(self, msg):
+        termcolor.cprint("COMP", "green")
+        sequence = Seq(msg.replace("COMP", "").strip())
+        comp = f"{sequence.complement()}"
+        print(comp)
+        return comp
+
+    def get_rev(self, msg):
+        termcolor.cprint("REV", "green")
+        sequence = Seq(msg.replace("REV", "").strip())
+        rev = f"{sequence.reverse()}"
+        print(rev)
+        return rev
 
 
 s = SeqServer()
