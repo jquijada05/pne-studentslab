428839427051fd9a54bccd52f0b038bde0c38289
jquijada05 <jquijada2005@gmail.com>
2024-05-09 22:15:30 +0200

    Commit

--- Final Project/server.py
+++ Final Project/server.py
@@ -5,6 +5,8 @@ from pathlib import Path
 import json
 from urllib.parse import parse_qs, urlparse
 import jinja2 as j
+from Seq1 import Seq
+
 # Define the Server's port
 PORT = 8080
 
@@ -16,7 +18,7 @@ def read_html_file(filename):
     return contents
 def get_json_object(object):
     Server = "rest.ensembl.org"
-    Endpoint = "/info" + object
+    Endpoint = object
     Params = "?content-type=application/json"
 
     conn = http.client.HTTPConnection(Server)
@@ -33,6 +35,13 @@ def get_json_object(object):
     json_object = json.loads(data)
     return json_object
 
+def get_info(sequence):
+    info = f"Total length: {sequence.len()}<br>"
+    for base in ["A", "C", "G", "T"]:
+        count = sequence.count_base(base)
+        percentage = count / sequence.len() * 100 if sequence.len() > 0 else 0
+        info += f"{base}: {count} ({percentage}%)<br>"
+    return info
 # Class with our Handler. It is a called derived from BaseHTTPRequestHandler
 # It means that our class inherits all his methods and properties
 class TestHandler(http.server.BaseHTTPRequestHandler):
@@ -52,17 +61,20 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
         # Read the index from the file
         if path == "/":
             contents = Path('index.html').read_text()
-        elif path == "/limit":
-            person = get_json_object("/species")
+        elif path == "/listSpecies":
+            person = get_json_object("/info/species")
             def get_limit_species(number):
                 limit_species = ""
                 for i in range(0, int(number)):
                     limit_species += "• " + person['species'][i]["display_name"] + "<br>"
                 return limit_species
-            contents = read_html_file('limit.html').render(context={"total_length": len(person['species']), "limit": arguments["limit"][0], "species": get_limit_species(arguments['limit'][0])})
+            if len(arguments) == 0:
+                contents = read_html_file('limit.html').render(context={"total_length": len(person['species']),"species": get_limit_species(317)})
+            else:
+                contents = read_html_file('limit.html').render(context={"total_length": len(person['species']), "limit": arguments["limit"][0], "species": get_limit_species(arguments['limit'][0])})
         elif path == "/karyotype":
             try:
-                person = get_json_object("/assembly/" + arguments["specie"][0])
+                person = get_json_object("/info/assembly/" + arguments["species"][0])
                 def get_karyotype():
                     karyotype = ""
                     for i in person['karyotype']:
@@ -71,15 +83,47 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
                 contents = read_html_file('karyotype.html').render(context={"karyotype": get_karyotype()})
             except KeyError:
                 contents = Path('error.html').read_text()
-        elif path == "/chromosome_length":
+        elif path == "/chromosomeLength":
             try:
-                person = get_json_object("/assembly/" + arguments["specie2"][0])
+                person = get_json_object("/info/assembly/" + arguments["species"][0])
                 def get_chromosome_length():
                     for i in person["top_level_region"]:
-                        if i["name"] == arguments["chromosome"][0]:
+                        if i["name"] == arguments["chromo"][0]:
                             length = i["length"]
                             return length
                 contents = read_html_file('chromosome_length.html').render(context={"chromosome_length": get_chromosome_length()})
+            except KeyError:
+                contents = Path('error.html').read_text()
+        elif path == "/geneSeq":
+            try:
+                person = get_json_object("/lookup/symbol/homo_sapiens/" + arguments["gene"][0])
+                gene_id = person.get("id")
+                sequence = get_json_object("/sequence/id/" + gene_id)
+                seq = sequence["seq"]
+                contents = read_html_file('geneSeq.html').render(context={"gene": arguments["gene"][0], "seq": seq})
+            except KeyError:
+                contents = Path('error.html').read_text()
+        elif path == "/geneInfo":
+            try:
+                person = get_json_object("/lookup/symbol/homo_sapiens/" + arguments["gene"][0])
+                gene_id = person.get("id")
+                sequence = get_json_object("/sequence/id/" + gene_id)
+                info = ""
+                info += f"Start: {person['start']}" + "<br>"
+                info += f"End: {person['end']}" + "<br>"
+                info += f"Length: {len(sequence['seq'])}" + "<br>"
+                info += f"ID: {person['id']}" + "<br>"
+                info += f"Chromosome: {person['seq_region_name']}"
+                contents = read_html_file('geneInfo.html').render(context={"gene": arguments["gene"][0], "info": info})
+            except KeyError:
+                contents = Path('error.html').read_text()
+        elif path == "/geneCalc":
+            try:
+                person = get_json_object("/lookup/symbol/homo_sapiens/" + arguments["gene"][0])
+                gene_id = person.get("id")
+                sequence = get_json_object("/sequence/id/" + gene_id)
+                s = Seq(sequence["seq"])
+                contents = read_html_file('geneCalc.html').render(context={"gene": arguments["gene"][0], "calc": get_info(s)})
             except KeyError:
                 contents = Path('error.html').read_text()
         else:
